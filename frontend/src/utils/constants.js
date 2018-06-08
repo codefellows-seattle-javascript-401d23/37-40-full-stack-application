7584e033c717b38c1492cd675ffc393da5faefcb
Sarah Geyer <sjgeyer@comcast.net>
2018-06-07 20:31:51 -0700

    email, password, and email validation added

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,4 @@
-export const COOKIE_TOKEN_KEY = 'PH-Token'; // eslint-disable-line
+export const COOKIE_TOKEN_KEY = 'PH-Token';
+export const DEF_USERNAME_ERR = 'Username is required';
+export const DEF_PASSWORD_ERR = 'Password is required';
+export const DEF_EMAIL_ERR = 'E-mail is required';
